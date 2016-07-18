e1e27ece01e6bb6b27943c70bfb7a515d726bb91
andy <andy@localhost.localdomain>
2016-07-18 17:26:23 -0500

    fixed topo

--- lb-topo.py
+++ lb-topo.py
@@ -21,6 +21,7 @@ class MyNet( Topo ):
         h1 = self.addHost( 'h1' )
         h2 = self.addHost( 'h2' )
         h3 = self.addHost( 'h3' )
+        h4= self.addHost('h4')
 
         # Add switches
         s1 = self.addSwitch( 's1' )
@@ -30,8 +31,9 @@ class MyNet( Topo ):
 
         # Add links
         self.addLink( h1, s1 )
-        self.addLink( h2, s2 )
-        self.addLink( h3, s3 )
+        self.addLink(h2,s1)
+        self.addLink( h3, s2 )
+        self.addLink( h4, s3 )
         self.addLink( s1, s2 )
         self.addLink( s2, s3 )
         self.addLink( s3, s1 )
